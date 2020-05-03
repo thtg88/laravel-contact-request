90686b693fd8592561af8d9ca05ba07171094ad2
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-03 21:22:37 +0100

    Added route prefix config support

--- config/laravel-contact-request.php
+++ config/laravel-contact-request.php
@@ -4,4 +4,5 @@ return [
     'recaptcha' => [
         'mode' => env('LARAVEL_CONTACT_REQUEST_RECAPTCHA_MODE', true),
     ],
+    'route_prefix' => env('LARAVEL_CONTACT_REQUEST_ROUTE_PREFIX', ''),
 ];
