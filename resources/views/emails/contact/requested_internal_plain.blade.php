bed02afadef2dbec349759598621a295fd1b1a60
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-13 21:27:03 +0100

    Display all data in emails

--- resources/views/emails/contact/requested_internal_plain.blade.php
+++ resources/views/emails/contact/requested_internal_plain.blade.php
@@ -1,6 +1,8 @@
 New contact request received.
 Details:
-Name: {{ $data['name'] }}
-Email: {{ $data['email'] }}
-Telephone Number: {{ $data['phone'] }}
-Message: {!! nl2br(htmlspecialchars($data['message'])) !!}
+@foreach ($data as $attribute => $value)
+    @if (! is_string($value))
+        @continue
+    @endif
+    {{ str_replace(['-', '_'], ' ', $attribute) }}: {!! nl2br(htmlspecialchars($data['message'], ENT_QUOTES, 'UTF-8')) !!}
+@endforeach
