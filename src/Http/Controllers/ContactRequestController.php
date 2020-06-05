d19ac7c7c3c7f370035e43f782c6034f05544a02
Marco Marassi <thtg88@users.noreply.github.com>
2020-06-05 20:10:53 +0100

    Add deliver later config option

--- src/Http/Controllers/ContactRequestController.php
+++ src/Http/Controllers/ContactRequestController.php
@@ -23,25 +23,36 @@ class ContactRequestController
     {
         $input = $request->validated();
 
+        $contact_requested_mail = new ContactRequested($input);
+        $contact_requested_internal_mail = new ContactRequestedInternal($input);
+
+        if (
+            Config::get('laravel-contact-request.mail.deliver_later') === true
+        ) {
+            // TODO: check for specific queue type and name config variables
+            $delivery_method = 'later';
+        } else {
+            $delivery_method = 'send';
+        }
+
         // Send email
         try {
             // Send confirmation mail to user
             Mail::to($input['email'])
-                ->send(new ContactRequested($input));
+                ->$delivery_method($contact_requested_mail);
 
             // Send internal notification
             Mail::to(Config::get(
                 'laravel-contact-request.mail.internal_notification_address'
-            ))->send(new ContactRequestedInternal($input));
+            ))->$delivery_method($contact_requested_internal_mail);
         } catch (Exception $e) {
             // TODO log errors?
             throw $e;
         }
 
-        return Container::getInstance()
-            ->make(ResponseFactory::class)->json([
-                'success' => true,
-                'contact_request' => $input,
-            ]);
+        return Container::getInstance()->make(ResponseFactory::class)->json([
+            'success' => true,
+            'contact_request' => $input,
+        ]);
     }
 }
