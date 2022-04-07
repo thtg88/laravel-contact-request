5b11917405c011dc52aeef0a8bc45f2c2d61576f
Marco Marassi <thtg88@users.noreply.github.com>
2022-04-07 21:33:53 +0100

    Introduce a ContactRequestService so it can be used independently from the controller's response

--- src/Http/Controllers/ContactRequestController.php
+++ src/Http/Controllers/ContactRequestController.php
@@ -5,11 +5,8 @@ namespace Thtg88\ContactRequest\Http\Controllers;
 use Exception;
 use Illuminate\Container\Container;
 use Illuminate\Routing\ResponseFactory;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Mail;
 use Thtg88\ContactRequest\Http\Requests\SubmitContactRequestRequest;
-use Thtg88\ContactRequest\Mail\ContactRequested;
-use Thtg88\ContactRequest\Mail\ContactRequestedInternal;
+use Thtg88\ContactRequest\Http\Services\ContactRequestService;
 
 class ContactRequestController
 {
@@ -22,32 +19,7 @@ class ContactRequestController
      */
     public function submit(SubmitContactRequestRequest $request)
     {
-        $input = $request->validated();
-
-        $contact_requested_mail = new ContactRequested($input);
-        $contact_requested_internal_mail = new ContactRequestedInternal($input);
-
-        if (Config::get('contact-request.mail.deliver_later') === true) {
-            // TODO: check for specific queue type and name config variables
-            $delivery_method = 'queue';
-        } else {
-            $delivery_method = 'send';
-        }
-
-        // Send email
-        try {
-            // Send confirmation mail to user
-            Mail::to($input['email'])
-                ->$delivery_method($contact_requested_mail);
-
-            // Send internal notification
-            Mail::to(Config::get(
-                'contact-request.mail.internal_notification_address'
-            ))->$delivery_method($contact_requested_internal_mail);
-        } catch (Exception $e) {
-            // TODO log errors?
-            throw $e;
-        }
+        $input = (new ContactRequestService())->submit($request);
 
         return Container::getInstance()->make(ResponseFactory::class)->json([
             'success'         => true,
